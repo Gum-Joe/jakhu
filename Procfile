6674a27abbdc51c391924eca345a9e83ec08d1c1
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-07-13 19:26:56 +0100

    Create Makefile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: api/programs/mongoDB/MongoDB/Server/3.0/bin/mongod.exe --dbpath ./api/data | node app.js
+web: make run
---
+++ run.heroku.sh
@@ -0,0 +1,5 @@
+# install bower modules
+echo Installing bower modules
+echo Installing bower
+npm install bower
+echo Installing components
