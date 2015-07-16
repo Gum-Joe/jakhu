b705dc7d7e846375ce93a58c1481fb601d687685
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-07-16 21:43:13 +0100

    Prepare for dev

--- app/console-app/src/Program.cs
+++ app/console-app/src/Program.cs
@@ -13,13 +13,13 @@ namespace console_app
             Console.WriteLine("Hello, and welcome to the web-os manager app");
             Console.WriteLine("To get started, please give us arguments");
             Console.WriteLine("Help");
-            if(args.Length == 0){
+            /*if(args.Length == 0){
                 Console.WriteLine("Help");
             } else if(args[1] == "install"){
                 scripts.install(args[2]);
             } else if(args[1] == "--help"){
                 help.showhelp("unspecified");
-            }
+            }*/
             foreach (string s in args)
         {
             System.Console.WriteLine(s);
