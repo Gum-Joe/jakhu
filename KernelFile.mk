03dc3dca892cf062e970aff409f4985db1a5749f
Gum Joe <kishansambhi@hotmail.co.uk>
2015-07-25 10:20:17 +0100

    Update KernelFile.mk

--- KernelFile.mk
+++ KernelFile.mk
@@ -3,6 +3,10 @@
 
 .CONFIGDIR:"kernel"
 
+__boot:$DEFAULTBOOTDIR
+
 config:
-TAB   $BOOTTYPE:1
-TAB   BOOTsCREEN
+TAB   $BOOTTYPE:1; \
+TAB   BOOTsCREEN:__boot"/boot.ejs
+TAB.  TAB.       b set bootstatus 1
+                 b set bootlabel "exec"
