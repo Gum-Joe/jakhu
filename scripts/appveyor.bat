1d10690f1cd6243796f6b600d320d0a99b51f51d
Gum Joe <kishansambhi@hotmail.co.uk>
2015-08-11 11:28:17 +0100

    Create appveyor.bat

---
+++ scripts/appveyor.bat
@@ -0,0 +1,17 @@
+echo Web-OS compiler + installer v1.0
+echo Copyright Gum-Joe
+echo This is for app veyor
+
+echo ""
+
+echo Installing bower and mocha
+npm install -g bower mocha
+
+echo Installing npm packages...
+npm install
+
+echo ""
+
+echo Compiling apps...
+cd app/console-app
+compile.bat
