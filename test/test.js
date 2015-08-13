7373ee89209af6946bd724da239a5c55e562300a
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-08-13 11:59:51 +0000

    add

--- test/test.js
+++ test/test.js
@@ -1,5 +1,12 @@
 describe('testing', function () {
-it('First test', function() {
-   console.log("Pending...")
+it('First test', function(done) {
+   process.env.TRAVIS_JOB_ID = -1;
+    process.env.COVERALLS_GIT_COMMIT = "GIT_HASH";
+    process.env.COVERALLS_GIT_BRANCH = "master";
+    process.env.COVERALLS_SERVICE_NAME = "SERVICE_NAME";
+    process.env.COVERALLS_SERVICE_JOB_ID = "SERVICE_JOB_ID";
+    process.env.COVERALLS_REPO_TOKEN = "REPO_TOKEN";
+   console.log("Pending...");
+  done();
   });
 });
