765c3ee66d07cd803fb1a3d0a38cc422265fcf3d
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-08-14 13:01:26 +0000

    Make sure ruby dosn't take over

--- instance-templates/ruby/rails/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'f85ece0df481b0699433f549a6cc0e79dba2992e60eae4c5fda79b048caf46134519446d6f42b4899c95fb526e4c0e8fa5f2a663aabb625fa034ab4adcf5f820';
