31411079efc8b863923b18aa1c5acfc37916f9d2
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-08-25 18:25:41 +0100

    Add stuff

--- boot/libs/checks/checkFile.js
+++ boot/libs/checks/checkFile.js
@@ -3,7 +3,7 @@ var path = require('path')
 var clicolour = require('cli-color')
 var error = require('../../../libs/error/bsod.js')
 
-var files = ["app.js", "node_modules"];
+var files = ["app.js", "node_modules", "libs"];
 
 var i;
 
@@ -37,6 +37,19 @@ exports.checkFiles = function(x){
             error.throwError("BOOT_CHECKS_FILES_"+err.code+":"+files[0], err, err.code);
               }
             });
+            fs.stat(files[2], function(err, stat){
+              if(err === null){
+                  console.log('Checked out file '+files[2]+"..."+clicolour.greenBright("OK"));
+                }
+                else if( err.code == 'ENOENT'){
+                  console.log('Checked out file '+files[2]+"..."+clicolour.redBright("ERROR!"));
+                  error.throwError("BOOT_"+err.code+"_CHECKS_NOT_FOUND:"+files[2], err, err.code);
+                }
+                else{
+                  console.log('Checked out file '+files[2]+"..."+clicolour.redBright("ERROR!"));
+                  error.throwError("BOOT_CHECKS_FILES_"+err.code+":"+files[2], err, err.code);
+                    }
+                  });
             fs.stat(files[1], function(err, stat){
               if(err === null){
                   console.log('Checked out file '+files[1]+"..."+clicolour.greenBright("OK"));
@@ -50,4 +63,5 @@ exports.checkFiles = function(x){
                   error.throwError("BOOT_CHECKS_FILES_"+err.code+":"+files[1], err, err.code);
                     }
                   });
+
     };
