13163fca78b22ce79ab935e3060242bba3098f4e
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-08-28 17:10:07 +0100

    RM .gitignores

--- boot/setup/setup.js
+++ boot/setup/setup.js
@@ -1,21 +1,28 @@
 var fs = require('fs');
 var clicolour = require('cli-color');
 var mkdirp = require('mkdirp');
+var error = require('../error/bsod.js')
 //var setup = require('')
 
 exports.first = function first(x) {
-  fs.stat('config', function(err, stat){
+    console.log(clicolour.cyanBright("webOS ") + clicolour.yellowBright("oobe ")+"Beginning Web-OS first time setup...");
+  fs.stat('../../config', function(err, stat){
     if(err === null){
         // do noting
       }
       else if( err.code == 'ENOENT'){
-        console.log(clicolour.cyanBright("webOS ") + clicolour.yellowBright("oobe ") + "Created config dir");
         // TODO: Add mkdrip config.
+        mkdirp('config', function(err) {
+          if (err) {
+            error.throwError("SETUP_CREATE_DIR_CONFIG", err, err.code);
+          } else {
+            console.log(clicolour.cyanBright("webOS ") + clicolour.yellowBright("oobe ") + "Created config dir");
+          }
+        });
 
       }
       else{
         error.throwError("BOOT_CHECKS_FILES_"+err.code+":"+"config", err, err.code);
           }
         });
-  console.log("Web-OS first time setup");
 }
