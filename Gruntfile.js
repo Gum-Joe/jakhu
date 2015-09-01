491f39ccde66ac306099c64530a038fcb653547b
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-09-01 11:08:37 +0100

    Ok

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
       test: {
         options: {
           reporter: 'spec',
-          captureFile: 'coveralls/cover.txt', // Optionally capture the reporter output to a file
+          captureFile: 'coverage/cover.txt', // Optionally capture the reporter output to a file
           quiet: false, // Optionally suppress output to standard out (defaults to false)
           clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
         },
