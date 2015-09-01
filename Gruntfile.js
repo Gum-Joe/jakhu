f75fdc8060449c912cc732912ecd911be66c4eba
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-09-01 10:52:34 +0100

    Create grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,4 +2,26 @@ var cli = require("cli-color");
 var sys = require('sys');
 var execute = require('child_process').exec;
 
-grunt.loadNpmTasks('grunt-mocha');
+module.exports = function(grunt) {
+
+  // Add the grunt-mocha-test tasks.
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+  grunt.initConfig({
+    // Configure a mochaTest task
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          captureFile: 'coveralls/cover.txt', // Optionally capture the reporter output to a file
+          quiet: false, // Optionally suppress output to standard out (defaults to false)
+          clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
+        },
+        src: ['test/*.js']
+      }
+    }
+  });
+
+  grunt.registerTask('default', 'mochaTest');
+
+};
