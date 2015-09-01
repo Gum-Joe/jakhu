888fd184c9391081adfc33d8c92bc2fd4f8caf3d
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-09-01 12:26:12 +0100

    Add to tests

--- test/boot/filechecks.js
+++ test/boot/filechecks.js
@@ -2,11 +2,9 @@
 var kernal = require('../../boot/index.js');
 
 var assert = require("assert");
-describe('Boot tests', function() {
-  describe('File test', function () {
+describe('File checks', function() {
     it('checks out file app.js', function (done) {
       kernal.checks.checkFiles();
       done();
     });
-  });
 });
