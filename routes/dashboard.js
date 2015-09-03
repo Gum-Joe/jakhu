f9b73e03269670ddcdd0caacda769cc20a4acbd4
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-09-03 19:06:56 +0100

    Add semi-colon

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var exec = require('child_process').exec;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   console.log("");
   res.render('dashboard/index.ejs', {
-    user: req.param("user")
+    user: req.param("user");
   });
 });
 
