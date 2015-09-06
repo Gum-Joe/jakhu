d2203d5f27d2fcbd5df4e084a00035ae099da5bd
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-09-06 18:45:05 +0100

    Finish adding

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.set('view engine', 'jade');
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'icon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
