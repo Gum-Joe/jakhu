33b9bbd5ef1c580ddd3ea4643c4509cb22bbcf4b
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-09-06 16:45:14 +0100

    Ok

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,7 @@ module.exports = function(grunt) {
 
   // Add the grunt-mocha-test tasks.
   grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-sass');
 
   grunt.initConfig({
     // Configure a mochaTest task
@@ -19,33 +20,33 @@ module.exports = function(grunt) {
         },
         src: ['test/*.js']
       }
+    },
+    sass: {                              // Task
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'expanded'
+        },
+        files: {                         // Dictionary of files
+          'views/css/dashboard.css': 'views/css/dashboard.scss',       // 'destination': 'source'
+        }
+      },
+      min: {                            // Target
+        options: {                       // Target options
+          style: 'compressed'
+        },
+        files: {                         // Dictionary of files
+          'views/css/dashboard.min.css': 'views/css/dashboard.scss',       // 'destination': 'source'
+        }
+      }
     }
   });
 
   grunt.registerTask('default', 'mochaTest');
 
-};
+  grunt.registerTask('compile:scss', 'sass:dist');
+  grunt.registerTask('compile:min', 'sass:min');
+  grunt.registerTask('compile', ['sass:min', 'sass:dist']);
 
-module.exports = function(grunt) {
-
-  // Add the grunt-mocha-test tasks.
-  grunt.loadNpmTasks('grunt-mocha-test');
-
-  grunt.initConfig({
-    // Configure a mochaTest task
-    mochaTest: {
-      test: {
-        options: {
-          reporter: 'spec',
-          captureFile: 'coverage/boot.txt', // Optionally capture the reporter output to a file
-          quiet: false, // Optionally suppress output to standard out (defaults to false)
-          clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
-        },
-        src: ['test/boot/*.js']
-      }
-    }
-  });
-
-  grunt.registerTask('boot:files', 'mochaTest');
+  // mochaTest
 
 };
