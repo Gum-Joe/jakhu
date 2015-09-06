4d6d1b5314f1277a04d4f2582f53aed4a8a30dd6
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-09-06 17:43:30 +0100

    Code Climate

--- boot/recovery/rollback.js
+++ boot/recovery/rollback.js
@@ -4,24 +4,8 @@ var clicolour = require("cli-color");
 var datea = "./recovery/rollback/backup";
 var exec = require('child_process').exec;
 
-var PythonShell = require('python-shell');
+//var PythonShell = require('python-shell');
 exports.createBackup = function createBackup(x){
-  mkdirp("./recovery", function (err) {
-    if(err){
-      error.throwError("RECOVERY_ROLLBACK_CREATE_BACKUP_DIR:"+err.code, err, err.code);
-    }
-    else {
-      console.log(clicolour.cyanBright("webOS ")  + clicolour.yellowBright("recovery ")  + "Created Recovery DIR");
-    };
-  });
-  mkdirp("./recovery/rollback", function (err) {
-    if(err){
-      error.throwError("RECOVERY_ROLLBACK_CREATE_BACKUP_DIR:"+err.code, err, err.code);
-    }
-    else {
-      console.log(clicolour.cyanBright("webOS ")  + clicolour.yellowBright("recovery ")  + "Created Rollback DIR");
-    };
-  });
   mkdirp(datea, function (err) {
     if(err){
       error.throwError("RECOVERY_ROLLBACK_CREATE_BACKUP_DIR:"+err.code, err, err.code);
