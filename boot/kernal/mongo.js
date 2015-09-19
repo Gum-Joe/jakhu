e8a5b0081c6b8511c60eb27213c79c6d3b48ea5b
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-09-19 12:09:12 +0100

    Add mongo

--- boot/kernal/mongo.js
+++ boot/kernal/mongo.js
@@ -15,7 +15,47 @@ exports.start = function start(callback) {
     // TODO: Make work in order
     fs.stat('./packages/mongo', function (err) {
       if(err){
-        var terminal = require('child_process').spawn('bash', ['./scripts/getmongo.sh']);
+        if(process.env.DOCKER === undefined || false){
+          if(os.type() === "Darwin"){
+            var terminal = require('child_process').spawn('bash', ['./scripts/getmongoosx.sh']);
+            terminal.stdout.on('data', function (data) {
+                console.log('stdout: ' + data);
+            });
+
+            terminal.stderr.on('data', function (data) {
+                console.log('stderr: ' + data);
+            });
+
+            terminal.on('exit', function (code) {
+                console.log('Done with '  + code);
+            });
+
+            setTimeout(function() {
+              console.log("Downloading MongoDB...");
+            }, 2000000);
+          } else if(os.type() === "Windows_NT"){
+            var terminal = require('child_process').spawn('bash', ['./scripts/getmongo.sh']);
+            terminal.stdout.on('data', function (data) {
+                console.log('stdout: ' + data);
+            });
+
+            terminal.stderr.on('data', function (data) {
+                console.log('stderr: ' + data);
+            });
+
+            terminal.on('exit', function (code) {
+                console.log('Done with '  + code);
+            });
+
+            setTimeout(function() {
+              console.log("Downloading MongoDB...");
+            }, 2000000);
+          } else {
+            console.log(clicolour.cyanBright("webOS ") + clicolour.magentaBright("database ") + "Sorry, you will need to start your own DB as their as so many distributions of Linux ");
+          };
+        } else {
+          var terminal = require('child_process').spawn('bash', ['docker run mongo']);
+        }
 
         terminal.stdout.on('data', function (data) {
             console.log('stdout: ' + data);
@@ -33,16 +73,24 @@ exports.start = function start(callback) {
           console.log("Downloading MongoDB...");
         }, 2000000);
       } else {
-        // stary DB
-        var m = require('child_process').spawn('bash', ['./scripts/mongo.sh']);
+        // start DB
+        if(process.env.DOCKER === undefined || false){
+          if(os.type() !== 'Linux'){
+            var m = require('child_process').spawn('bash', ['./scripts/mongo.sh']);
 
-        m.on('exit', function (code) {
-            console.log(clicolour.cyanBright("webOS ") + clicolour.magentaBright("database ") + "Database started with "+code);
-        });
+            m.on('exit', function (code) {
+                console.log(clicolour.cyanBright("webOS ") + clicolour.magentaBright("database ") + "Database started with "+code);
+            });
 
-        setTimeout(function() {
-          console.log("Starting MongoDB...");
-        }, 2000000);
+            setTimeout(function() {
+              console.log("Starting MongoDB...");
+            }, 2000000);
+          } else {
+            console.log(clicolour.cyanBright("webOS ") + clicolour.magentaBright("database ") + "Sorry, but MongoDB is not stored locally as it was not downloaded");
+          };
+        } else {
+          var terminal = require('child_process').spawn('bash', ['docker run mongo']);
+        };
       };
     });
   } else {
