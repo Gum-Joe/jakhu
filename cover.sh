4044a95400330618bd5d3b0d3eda28f5504f955e
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-09-21 17:47:30 +0100

    Add code_climate

--- cover.sh
+++ cover.sh
@@ -1,3 +1,3 @@
 istanbul cover ./node_modules/mocha/bin/_mocha test/**/*.js --reporter=lcovonly -- -R spec && cat coverage/lcov.info | node_modules/.bin/coveralls
 # report to codeclimate-test-reporter
-node_modules/.bin/code-code-climate-test-reporter < coverage/lcov.info
+node_modules/.bin/codeclimate-test-reporter < coverage/lcov.info
