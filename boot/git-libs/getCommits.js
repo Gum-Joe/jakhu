d403aa543a4e61b4e6a4861dbf20c2b34daec09d
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-09-27 17:41:36 +0100

    Rm unused method

--- boot/git-libs/getCommits.js
+++ boot/git-libs/getCommits.js
@@ -1,8 +1,8 @@
 var exec = require('child_process').exec;
 
-exports.getCommits = function (x) {
+/**exports.getCommits = function (x) {
   exec("git rev-list HEAD --count", function (error, stdout, stderr) {
       return stdout;
       return stderr;
   });
-}
+}*/
