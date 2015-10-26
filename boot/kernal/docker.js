ad413590cd1615e90da3a31c758816779cdd96b3
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-10-26 15:15:41 +0000

    Keep building

--- boot/kernal/docker.js
+++ boot/kernal/docker.js
@@ -1,10 +1,30 @@
 // Starts the required docker stuff
+var YAML = require('yaml-js');
+var YAM = require('yamljs');
+var fs = require('fs');
+var tub = require('web-os-container');
 function boot() {
   // body...
-  // Load mongodb
-  if (process.env.WEB_DOCKER_I === undefined) {
-    throw new error('h')
+  // Check if a docker ip is set
+  var docker = YAML.load(fs.readFileSync('config/docker.yml'));
+  if (docker.ip === undefined) {
+    if (process.env.WEB_DOCKER_IP === undefined && process.env.WEB_DOCKER !== undefined || false) {
+      throw new Error('Your docker vm ip address is not set or set to the wrong env! Please set it as the enviroment variable "WEB_DOCKER_IP"')
+    };
+  } else {
+    // Load Docker
+    // Pull docker image
+    var datew = new Date().getDate();
+    console.log(datew);
+    if (docker.date === undefined) {
+      console.log('Pulling images...');
+      tub.pullImages('config/docker.yml', true);
+      var imi = docker;
+      imi.date = datew
+      var da = YAM.stringify(imi, 4);
+      fs.writeFile('config/docker.yml', da);
+    }
   }
-}
+};
 module.exports = {boot: boot};
-boot()
+boot();
