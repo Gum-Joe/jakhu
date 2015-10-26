7f20a42380a0c227c0128015dc946dee89686ef2
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-10-26 13:02:22 +0000

    Re write build.cmd

---
+++ boot/kernal/docker.js
@@ -0,0 +1,10 @@
+// Starts the required docker stuff
+function boot() {
+  // body...
+  // Load mongodb
+  if (process.env.WEB_DOCKER_I === undefined) {
+    throw new error('h')
+  }
+}
+module.exports = {boot: boot};
+boot()
