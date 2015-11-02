e6d68a42348f15bc869e98d77850a50e43a53177
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-11-02 17:38:34 +0000

    Begin side bar

--- views/js/hamburger.js
+++ views/js/hamburger.js
@@ -17,3 +17,24 @@
   }
 
 })();
+
+$(window).on('scroll',
+	{
+	    previousTop: 0
+	},
+	function () {
+	    var currentTop = $(window).scrollTop();
+	    //check if user is scrolling up
+	    if (currentTop < this.previousTop ) {
+	    	//if scrolling up...
+	    	//add class 'is-visible' to the main navigation
+	    	//if currentTop == 0, remove 'is-fixed' and 'is-visible' classes
+	    } else {
+	    	//if scrolling down...
+	    	//add the 'is-fixed' class to the main navigation as soon as it is no longer visible
+	    	//add class 'is-visible' to the main navigation
+	    }
+	    //set previousTop for the next iteration
+	    this.previousTop = currentTop;
+	}
+);
