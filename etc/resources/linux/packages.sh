41a1d8892295297c99e57213394faf63d1f17ba2
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-11-28 10:38:16 +0000

    Use old makefile
    We have got more important things to do

--- etc/resources/linux/packages.sh
+++ etc/resources/linux/packages.sh
@@ -1,2 +1,16 @@
-#!/usr/bin/bash
 #!/bin/bash
+echo "Updating package db.."
+echo "+ apt-get update"
+sudo apt-get update
+if [[ ! "git --version" ]]; then
+  #statements
+  echo "Installing git.."
+  echo "+ apt-get install git-core"
+  sudo apt-get install git-core
+fi
+if [[ ! "curl --version" ]]; then
+  #statements
+  echo "Installing curl.."
+  echo "+ apt-get install curl"
+  sudo apt-get install git-core
+fi
