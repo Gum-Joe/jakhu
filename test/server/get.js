4cb62179fabf3c407c1ae4186d1d673d3381100e
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-11-30 20:05:31 +0000

    test fix

--- test/server/get.js
+++ test/server/get.js
@@ -11,7 +11,7 @@ describe('GET requests', function (done) {
     done();
   });
   it('GET /', function (done) {
-    request("http://localhost:6060")
+    request("http://localhost:9090")
       .get('/')
       .set('Accept', 'text/html')
       .expect('Content-Type', /html/)
@@ -19,7 +19,7 @@ describe('GET requests', function (done) {
     done();
   });
   it('GET /dashboard', function (done) {
-    request("http://localhost:6060")
+    request("http://localhost:9090")
       .get('/dashboard')
       .set('Accept', 'text/html')
       .expect('Content-Type', /html/)
@@ -27,7 +27,7 @@ describe('GET requests', function (done) {
     done();
   });
   it('GET /start', function (done) {
-    request("http://localhost:6060")
+    request("http://localhost:9090")
       .get('/start')
       .set('Accept', 'text/html')
       .expect('Content-Type', /html/)
@@ -35,7 +35,7 @@ describe('GET requests', function (done) {
     done();
   });
   it('GET /signin', function (done) {
-    request("http://localhost:6060")
+    request("http://localhost:9090")
       .get('/signin')
       .set('Accept', 'text/html')
       .expect('Content-Type', /html/)
@@ -43,7 +43,7 @@ describe('GET requests', function (done) {
     done();
   });
   it('GET /license', function (done) {
-    request("http://localhost:6060")
+    request("http://localhost:9090")
       .get('/license')
       .set('Accept', 'text/html')
       .expect('Content-Type', /html/)
@@ -51,7 +51,7 @@ describe('GET requests', function (done) {
     done();
   });
   it('GET /decline', function (done) {
-    request("http://localhost:6060")
+    request("http://localhost:9090")
       .get('/decline')
       .set('Accept', 'text/html')
       .expect('Content-Type', /html/)
@@ -59,7 +59,7 @@ describe('GET requests', function (done) {
     done();
   });
   it('GET /decline-y', function (done) {
-    request("http://localhost:6060")
+    request("http://localhost:9090")
       .get('/decline-y')
       .set('Accept', 'text/html')
       .expect('Content-Type', /html/)
@@ -67,7 +67,7 @@ describe('GET requests', function (done) {
     done();
   });
   it('GET /decline-n', function (done) {
-    request("http://localhost:6060")
+    request("http://localhost:9090")
       .get('/decline-n')
       .set('Accept', 'text/html')
       .expect('Content-Type', /html/)
@@ -75,7 +75,7 @@ describe('GET requests', function (done) {
     done();
   });
   it('GET /accept', function (done) {
-    request("http://localhost:6060")
+    request("http://localhost:9090")
       .get('/accept')
       .set('Accept', 'text/html')
       .expect('Content-Type', /html/)
@@ -83,7 +83,7 @@ describe('GET requests', function (done) {
     done();
   });
   it('GET /wosl', function (done) {
-    request("http://localhost:6060")
+    request("http://localhost:9090")
       .get('/wosl')
       .set('Accept', 'text/html')
       .expect('Content-Type', /html/)
@@ -91,7 +91,7 @@ describe('GET requests', function (done) {
     done();
   });
   it('GET /declinew', function (done) {
-    request("http://localhost:6060")
+    request("http://localhost:9090")
       .get('/declinew')
       .set('Accept', 'text/html')
       .expect('Content-Type', /html/)
@@ -99,7 +99,7 @@ describe('GET requests', function (done) {
     done();
   });
   it('GET /declinew-y', function (done) {
-    request("http://localhost:6060")
+    request("http://localhost:9090")
       .get('/declinew-y')
       .set('Accept', 'text/html')
       .expect('Content-Type', /html/)
@@ -107,7 +107,7 @@ describe('GET requests', function (done) {
     done();
   });
   it('GET /declinew-n', function (done) {
-    request("http://localhost:6060")
+    request("http://localhost:9090")
       .get('/declinew-n')
       .set('Accept', 'text/html')
       .expect('Content-Type', /html/)
@@ -115,7 +115,7 @@ describe('GET requests', function (done) {
     done();
   });
   it('GET /acceptw', function (done) {
-    request("http://localhost:6060")
+    request("http://localhost:9090")
       .get('/acceptw')
       .set('Accept', 'text/html')
       .expect('Content-Type', /html/)
@@ -123,7 +123,7 @@ describe('GET requests', function (done) {
     done();
   });
   it('GET /type', function (done) {
-    request("http://localhost:6060")
+    request("http://localhost:9090")
       .get('/type')
       .set('Accept', 'text/html')
       .expect('Content-Type', /html/)
@@ -131,7 +131,7 @@ describe('GET requests', function (done) {
     done();
   });
   it('GET /set-lang', function (done) {
-    request("http://localhost:6060")
+    request("http://localhost:9090")
       .get('/set-lang')
       .set('Accept', 'text/html')
       .expect('Content-Type', /html/)
@@ -139,7 +139,7 @@ describe('GET requests', function (done) {
     done();
   });
   it('GET /install-standard', function (done) {
-    request("http://localhost:6060")
+    request("http://localhost:9090")
       .get('/install-standard')
       .set('Accept', 'text/html')
       .expect('Content-Type', /html/)
@@ -147,7 +147,7 @@ describe('GET requests', function (done) {
     done();
   });
   it('GET /set-install-standard', function (done) {
-    request("http://localhost:6060")
+    request("http://localhost:9090")
       .get('/set-install-standard')
       .set('Accept', 'text/html')
       .expect('Content-Type', /html/)
@@ -155,7 +155,7 @@ describe('GET requests', function (done) {
     done();
   });
   it('GET /opt-standard', function (done) {
-    request("http://localhost:6060")
+    request("http://localhost:9090")
       .get('/opt-standard')
       .set('Accept', 'text/html')
       .expect('Content-Type', /html/)
@@ -163,7 +163,7 @@ describe('GET requests', function (done) {
     done();
   });
   it('GET /recovery', function (done) {
-    request("http://localhost:6060")
+    request("http://localhost:9090")
       .get('/recovery')
       .set('Accept', 'text/html')
       .expect('Content-Type', /html/)
@@ -171,7 +171,7 @@ describe('GET requests', function (done) {
     done();
   });
   /*it('should test the reply of GET /users', function (done) {
-    http.get("http://localhost:6060/users", function(res) {
+    http.get("http://localhost:9090/users", function(res) {
       console.log(res.reply);
         res.on('data', function(body) {
             console.log(body)
