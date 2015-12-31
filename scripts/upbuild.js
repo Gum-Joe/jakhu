4149c1dbf969ce99e4c95eadd9040e4dfdbee094
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-12-31 11:35:11 +0000

    Make increment

--- scripts/upbuild.js
+++ scripts/upbuild.js
@@ -5,5 +5,7 @@ var exec = require('child_process').exec;
 exec("git rev-list HEAD --count", function (error, stdout, stderr) {
   var com = parseInt(stdout) - parseInt(pack.version.slice(4))
   console.log(com);
-
+  for (var i = 0; i < com; i++) {
+    exec("npm version minor")
+  }
 });
