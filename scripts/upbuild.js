3ad43cdd8006e18c5493f204b85bba089e68e005
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-12-31 11:36:30 +0000

    Ignore date.txt

--- scripts/upbuild.js
+++ scripts/upbuild.js
@@ -1,11 +1,11 @@
 // Increment builds
 var pack = require('../package.json');
-var exec = require('child_process').exec;
+var exec = require('child_process');
 
-exec("git rev-list HEAD --count", function (error, stdout, stderr) {
+exec.exec("git rev-list HEAD --count", function (error, stdout, stderr) {
   var com = parseInt(stdout) - parseInt(pack.version.slice(4))
   console.log(com);
   for (var i = 0; i < com; i++) {
-    exec("npm version minor")
+    exec.execSync("npm version minor")
   }
 });
