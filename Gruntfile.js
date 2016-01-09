644b235909ad90500589967c46fe8e9284e23d28
Gum-Joe <kishansambhi@hotmail.co.uk>
2016-01-09 21:09:29 +0000

    Chnage background

--- Gruntfile.js
+++ Gruntfile.js
@@ -121,6 +121,8 @@ module.exports = function(grunt) {
           'views/css/oobe.css': 'views/src/oobe.scss',       // 'destination': 'source'
           'views/css/float.css': 'views/src/float.scss',
           'views/css/dashboard-mobile.css': 'views/src/dashboard-mobile.scss',
+          'views/css/dashboard-mobile-table-331.css': 'views/src/dashboard-mobile-table-331.scss',
+          'views/css/dashboard-mobile-table-320.css': 'views/src/dashboard-mobile-table-320.scss',
           'views/css/dashboard-mobile-apple.css': 'views/src/dashboard-mobile-apple.scss',
           'views/css/dashboard-tablet.css': 'views/src/dashboard-tablet.scss',
           'views/css/dashboard-main.css': 'views/src/dashboard-main.scss',
