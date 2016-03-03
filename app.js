d46bd59d3be82ec758fdc14ccb38b069f8f72a6f
Gum-Joe <kishansambhi@hotmail.co.uk>
2016-03-03 20:24:05 +0000

    :file_folder: Move public -> views/public
    :large_

--- app.js
+++ app.js
@@ -59,13 +59,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'icon.png')));
+// uncomment after placing your favicon in /views/public
+app.use(favicon(path.join(__dirname, 'views/public', 'icon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views/public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use(express.static(path.join(__dirname, 'views')));
